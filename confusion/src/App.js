8056efb5d67ec7d282e2565db73f97b66a511a85
aayushrai <www.aayushrai@gmail.com>
2020-08-23 17:59:14 +0530

    create Card

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import { Navbar,NavbarBrand } from 'reactstrap';
 import Menu from "./Components/MenuComponents"
 import './App.css';
+import {DISHES} from './shared/dishes';
+
 
 function App() {
+  const state = {
+    dishes: DISHES
+  };
   return (
-    <div >
+    <div>
       <Navbar dark color='primary'>
         <div className="container">
           <NavbarBrand href="/">Arms</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={state.dishes} />
     </div>
   );
 }
