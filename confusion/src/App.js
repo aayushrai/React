a74f3408ffb7b4710d074f0b62c2a910af210844
aayushrai <www.aayushrai@gmail.com>
2020-08-27 00:49:23 +0530

    main component

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Navbar,NavbarBrand } from 'reactstrap';
-import Menu from "./Components/MenuComponents"
+import Main from "./Components/MainComponent"
 import './App.css';
-import {DISHES} from './shared/dishes';
 
 
 function App() {
-  const state = {
-    dishes: DISHES
-  };
+
   return (
     <div>
-      <Navbar dark color='primary'>
-        <div className="container">
-          <NavbarBrand href="/">Arms</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={state.dishes} />
+      <Main />
     </div>
   );
 }
--- confusion/src/Components/DishdetailComponent.js
+++ confusion/src/Components/DishdetailComponent.js
@@ -1,22 +1,8 @@
 import React,{Component} from 'react'
-import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 'reactstrap'
+import {Card,CardImg,CardText,CardBody,CardTitle} from 'reactstrap'
 
 class Dishdetail extends Component{
-    constructor(props) {
-        super(props);
-        this.state ={
-            selectedDish: null
-        }
-        console.log("Menu constructor call");
-    }
 
-    componentDidMount(){
-        console.log("Menu componentDidMount call");
-    };
-    onDishSelect(dish){
-        this.setState({selectedDish:dish});
-    }
-   
 renderDish(dish){
        
     if(dish!=null){
@@ -69,29 +55,15 @@ renderComment(dish){
     }
     }
 render(){
-    const menu = this.props.dishes.map((dish) => {
-            return (
-              <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
-                      <CardImg width="100%" object src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                     <CardTitle>{dish.name}</CardTitle> 
-                  </CardImgOverlay>
-                </Card>
-              </div>
-            );
-        });
         console.log("Menu Render call");
         return (
-          <div className="container">
+            <div className="container">
             <div className="row">
-                  {menu}
+                    {this.renderDish(this.props.dish)}    
+                    {this.renderComment(this.props.dish)}
             </div>
-            <div className="row">
-                    {this.renderDish(this.state.selectedDish)}    
-                    {this.renderComment(this.state.selectedDish)}
             </div>
-          </div>
+            
         );
 }
 }
