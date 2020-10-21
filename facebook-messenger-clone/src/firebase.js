eb935622176480779ea320bcca53fd1d9d653459
aayushrai <www.aayushrai@gmail.com>
2020-10-21 20:17:16 +0530

    amazon clone

--- facebook-messenger-clone/src/firebase.js
+++ facebook-messenger-clone/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAdFQLdjGSdZvigcOLFtcPiWcgIXIuLEu8",
+ apiKey: "AIzaSyAdFQLdjGSdZvigcOLFtcPiWcgIXIuLEu8",
   authDomain: "facebook-messenger-clone-ca575.firebaseapp.com",
   databaseURL: "https://facebook-messenger-clone-ca575.firebaseio.com",
   projectId: "facebook-messenger-clone-ca575",
   storageBucket: "facebook-messenger-clone-ca575.appspot.com",
   messagingSenderId: "700666149684",
-  appId: "1:700666149684:web:0b7782ccc27a294088c361",
-  measurementId: "G-069N4Y1M4H"
+  appId: "1:700666149684:web:90539773c3c1096d88c361",
+  measurementId: "G-SKR6VT8PWZ"
 
 });
 const db = firebaseApp.firestore();
